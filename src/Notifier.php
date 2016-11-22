8092086087746a1d5ab4c0375f409665b1af254a
Josselin Favennec <jfavennec@lefigaro.fr>
2016-11-22 18:53:57 +0100

    Validate the meteor url.

--- src/Notifier.php
+++ src/Notifier.php
@@ -68,21 +68,68 @@ class Notifier {
   public function notify($path, $query = []) {
     $host = $this->config->get('meteor_server');
     $url = rtrim($host, '/') . '/' . ltrim($path, '/');
-    $valid_url = filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED);
-    if (!$valid_url) {
+    if (!$this->isUrlValid($url)) {
       $this->logger->error("Can't notify meteor on @url (invalid url).", [
         '@url' => $url,
       ]);
+      return;
     }
-    $promise = $this->client->getAsync($valid_url, ['query' => $query]);
+    $promise = $this->client->getAsync($url, ['query' => $query]);
     $this->logger->info("Notify meteor on @url with query @query.", [
-      '@url' => $valid_url,
+      '@url' => $url,
       '@query' => $query ? var_export($query, TRUE) : '(empty)',
     ]);
     drupal_register_shutdown_function(function () use ($promise) {
       $this->finalizeWait($promise);
     });
 
+  }
+
+  /**
+   * Meteor url validation.
+   *
+   * From https://gist.github.com/dperini/729294
+   *
+   * @param string  $url
+   *   The url to validate.
+   * @return bool
+   *   If the url is valide or not.
+   */
+  public function isUrlValid($url) {
+    $valid_url_regex = '_^' .
+      // protocol identifier
+      '(?:https?://)' .
+      // user:pass authentication
+      '(?:\S+(?::\S*)?@)?' .
+      '(?:' .
+        // IP address exclusion
+        // private & local networks
+        '(?!(?:10|127)(?:\.\d{1,3}){3})' .
+        '(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})' .
+        '(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})' .
+        // IP address dotted notation octets
+        // excludes loopback network 0.0.0.0
+        // excludes reserved space >= 224.0.0.0
+        // excludes network & broacast addresses
+        // (first & last IP address of each class)
+        '(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])' .
+        '(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}' .
+        '(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))' .
+      '|' .
+        // host name
+        '(?:(?:[a-z\x{00a1}-\x{ffff}0-9]-*)*[a-z\x{00a1}-\x{ffff}0-9]+)' .
+        // domain name
+        '(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]-*)*[a-z\x{00a1}-\x{ffff}0-9]+)*' .
+        // TLD identifier
+        '(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,}))' .
+      ')' .
+      // port number
+      '(?::\d{2,5})?' .
+      // resource path (we prevent query params and anchors)
+      '(?:/[^\s\?#]*)?' .
+      '$_iuS';
+
+    return preg_match($valid_url_regex, $url) ? TRUE : FALSE;
   }
 
   /**
